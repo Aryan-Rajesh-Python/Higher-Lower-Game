c23546fa42a270e7d10463a8cf9fad8ac16b2ac8
Aryan Rajesh <122169573+Aryan-Rajesh-Python@users.noreply.github.com>
2023-01-08 00:00:33 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+data=[
+{
+    "Name":"Christiano Ronaldo",
+    "Followers":515,
+    "Occupation":"Football Player"
+},
+{
+    "Name":"Lionel Messi",
+    "Followers":415,
+    "Occupation":"Football Player"
+},
+{
+    "Name":"Ariana Grande",
+    "Followers":365,
+    "Occupation":"Singer"
+},
+{
+    "Name":"Kim kardashian",
+    "Followers":350,
+    "Occupation":"TV Actress and Entrepreneur"
+},
+{
+    "Name":"Virat Kohli",
+    "Followers":220,
+    "Occupation":"Cricketer"
+}   
+]
